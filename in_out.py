a0f82a3e1d0503918eb23c6e054c4586b2baa836
rodrigo <pv26050@estgl.ipv.pt>
2022-10-21 13:26:57 +0100

    in_out

---
+++ in_out.py
@@ -0,0 +1,16 @@
+# input a float value for variable a here
+
+a= float(input("Introduz um numero para a"))
+print ("introduziu", a, "para a")
+# input a float value for variable b here
+
+b= float(input("introduz um numero para b"))
+print ("introduziu", b, "para b")
+
+# output the result of addition here
+print(a, "+",b, "=", a+b)
+# output the result of subtraction here
+# output the result of multiplication here
+# output the result of division here
+
+print("\nThat's all, folks!")
